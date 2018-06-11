1ab88241059101186c19e1c6f9d4cdee6c0890c7
Fabian Winnen <fabian@kinemic.de>
2018-06-11 10:47:53 +0200

    added differend r17 apis

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -156,10 +156,20 @@ toolchain_table = [
     Toolchain('android-ndk-r16b-api-24-armeabi-v7a-neon-clang-libcxx14', 'Unix Makefiles'),
     Toolchain('android-ndk-r17-api-16-armeabi-v7a-clang-libcxx14', 'Unix Makefiles'),
 
+    Toolchain('android-ndk-r17-api-21-arm64-v8a-clang-libcxx14', 'Unix Makefiles'),
+    Toolchain('android-ndk-r17-api-21-armeabi-v7a-clang-libcxx14', 'Unix Makefiles'),
+    Toolchain('android-ndk-r17-api-21-x86-64-clang-libcxx14', 'Unix Makefiles'),
+    Toolchain('android-ndk-r17-api-21-x86-clang-libcxx14', 'Unix Makefiles'),
+
     Toolchain('android-ndk-r17-api-24-arm64-v8a-clang-libcxx14', 'Unix Makefiles'),
     Toolchain('android-ndk-r17-api-24-armeabi-v7a-clang-libcxx14', 'Unix Makefiles'),
     Toolchain('android-ndk-r17-api-24-x86-64-clang-libcxx14', 'Unix Makefiles'),
     Toolchain('android-ndk-r17-api-24-x86-clang-libcxx14', 'Unix Makefiles'),
+
+    Toolchain('android-ndk-r17-api-27-arm64-v8a-clang-libcxx14', 'Unix Makefiles'),
+    Toolchain('android-ndk-r17-api-27-armeabi-v7a-clang-libcxx14', 'Unix Makefiles'),
+    Toolchain('android-ndk-r17-api-27-x86-64-clang-libcxx14', 'Unix Makefiles'),
+    Toolchain('android-ndk-r17-api-27-x86-clang-libcxx14', 'Unix Makefiles'),
 
     Toolchain('emscripten-cxx11', 'Unix Makefiles'),
     Toolchain('raspberrypi1-cxx11-pic', 'Unix Makefiles'),
