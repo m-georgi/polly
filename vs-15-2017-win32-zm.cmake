1186f8f2642c89695a35153f21022990e66c6c22
Marcus Georgi <marcus.georgi@kinemic.de>
2019-09-04 10:52:04 +0200

    fix win32 zm path

--- vs-15-2017-win32-zm.cmake
+++ vs-15-2017-win32-zm.cmake
@@ -18,4 +18,4 @@ include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 
 include(polly_add_cache_flag)
 polly_add_cache_flag(CMAKE_CXX_FLAGS_INIT "-DNOMINMAX")
-polly_add_cache_flag(CMAKE_VS_SDK_EXECUTABLE_DIRECTORIES "$(VC_ExecutablePath_x86_x64);$(WindowsSDK_ExecutablePath);$(VS_ExecutablePath);$(MSBuild_ExecutablePath);$(FxCopDir);$(PATH);")
+polly_add_cache_flag(CMAKE_VS_SDK_EXECUTABLE_DIRECTORIES "$(VC_ExecutablePath_x64_x86);$(WindowsSDK_ExecutablePath);$(VS_ExecutablePath);$(MSBuild_ExecutablePath);$(SystemRoot)\SysWow64;$(FxCopDir);$(PATH);")
